160683658769dee85f4d3c462426d9dfa8a44804
EamonH <eamon@Eamons-MacBook-Pro.local>
2018-01-23 22:51:14 +0000

    Fix categories in admin.

--- rango/admin.py
+++ rango/admin.py
@@ -3,11 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from rango.models import Category, Page
 
-admin.site.register(Category)
-
-
 class PageAdmin(admin.ModelAdmin):
 	list_display = ('title', 'category', 'url')
 
+class CatAdmin(admin.ModelAdmin):
+	list_display = ('name', 'views', 'likes')
 
 admin.site.register(Page, PageAdmin)
+admin.site.register(Category, CatAdmin)
